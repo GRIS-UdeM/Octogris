58fba9a4c59f87bc604e68958312fc76718c14aa
Vincent Berthiaume <vberthiaume@gmail.com>
2014-11-18 14:49:14 -0500

    changed base sdk to 10.9

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -34,8 +34,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Octogris";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "2.0.10";
+    const int          versionNumber  = 0x2000a;
 }
 #endif
 
