d12be2aff403a2369195cf3c3063222f60ac63ed
Landrieu <antoinelandrieu@gmail.com>
2015-04-07 12:12:43 -0400

    Octogris avec joystick et LeapMotion

---
+++ HIDManager/HID_Queue_Utilities.h
@@ -0,0 +1,14 @@
+//
+//  HID_Queue_Utilities.h
+//  HIDManager8
+//
+//  Created by GRIS on 2015-02-25.
+//  Copyright (c) 2015 Landrieu Antoine. All rights reserved.
+//
+
+#ifndef __HIDManager8__HID_Queue_Utilities__
+#define __HIDManager8__HID_Queue_Utilities__
+
+#include <iostream>
+
+#endif /* defined(__HIDManager8__HID_Queue_Utilities__) */
---
+++ HIDManager/HID_Utilities.h
@@ -0,0 +1,14 @@
+//
+//  HID_Utilities.h
+//  HIDManager8
+//
+//  Created by GRIS on 2015-02-25.
+//  Copyright (c) 2015 Landrieu Antoine. All rights reserved.
+//
+
+#ifndef __HIDManager8__HID_Utilities__
+#define __HIDManager8__HID_Utilities__
+
+#include <iostream>
+
+#endif /* defined(__HIDManager8__HID_Utilities__) */
