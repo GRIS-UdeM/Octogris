bca17633a41f46bc14b06fb1f62211dd0012a077
Vincent Berthiaume <vberthiaume@gmail.com>
2015-12-04 15:20:19 -0500

    changed version to 2.2.7

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -185,13 +185,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                2.2.6
+ #define JucePlugin_Version                2.2.7
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0x20206
+ #define JucePlugin_VersionCode            0x20207
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "2.2.6"
+ #define JucePlugin_VersionString          "2.2.7"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
