2859e432d43d99d214f9e139e7ebe726ff15c7d1
Vincent Berthiaume <vberthiaume@gmail.com>
2016-02-06 11:51:14 -0500

    fix #106

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -2023,7 +2023,7 @@ void OctogrisAudioProcessorEditor::timerCallback()
 			} else {
 				mTrWriteButton->setButtonText("Ready");
                 mTrWriteButton->setToggleState(false, dontSendNotification);
-                mFilter->restoreCurrentLocations(mFilter->getSrcSelected());
+                mFilter->restoreCurrentLocations(-1);
 				mTrProgressBar->setVisible(false);
                 mTrStateEditor = kTrReady;
 				mFilter->setTrState(mTrStateEditor);
