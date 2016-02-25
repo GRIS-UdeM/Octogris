1952c526f4a35947af6ebbe3da293a7038c0dd47
Vincent Berthiaume <vberthiaume@gmail.com>
2016-02-25 13:55:08 -0500

    update copyPlugin script with major version 3

--- copyPlugins.sh
+++ copyPlugins.sh
@@ -3,11 +3,11 @@
 VERSION="$1"
 
 pushd ~/Library/Audio/Plug-Ins/Components/
-zip -r ~/Desktop/Octogris$VERSION.zip ./Octogris2.component 
+zip -r ~/Desktop/Octogris$VERSION.zip ./Octogris3.component 
 popd
 
 pushd ~/Library/Audio/Plug-Ins/VST/
-zip -r ~/Desktop/Octogris$VERSION.zip ./Octogris2.vst 
+zip -r ~/Desktop/Octogris$VERSION.zip ./Octogris3.vst 
 popd
 
 #zip -rj ~/Desktop/ZirkOSC.zip ~/Library/Audio/Plug-Ins/Components/ZirkOSC3.component ~/Library/Audio/Plug-Ins/VST/ZirkOSC3.vst 
